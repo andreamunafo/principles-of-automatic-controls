b55b61af249d1bc2b0d160a72d7cf30f4b86857e
Andrea Munafo <andrea.munafo@gmail.com>
2023-12-31 13:12:45 +0100

    Update

--- principles_of_automatic_controls/_modidx.py
+++ principles_of_automatic_controls/_modidx.py
@@ -5,5 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://andreamunafo.github.io',
                 'git_url': 'https://github.com/andreamunafo/principles-of-automatic-controls',
                 'lib_path': 'principles_of_automatic_controls'},
-  'syms': { 'principles_of_automatic_controls.core': { 'principles_of_automatic_controls.core.foo': ( 'core.html#foo',
-                                                                                                      'principles_of_automatic_controls/core.py')}}}
+  'syms': { 'principles_of_automatic_controls.introduction_to_control_problem': { 'principles_of_automatic_controls.introduction_to_control_problem.draw_mass_spring_damper': ( 'introduction_to_control_problem.html#draw_mass_spring_damper',
+                                                                                                                                                                                'principles_of_automatic_controls/introduction_to_control_problem.py')}}}
